d7f95e2627a075008ab820ea7e03d41c8b623d8f
Bryan Taboada <b.taboada123@gmail.com>
2024-08-03 15:19:55 -0600

    added model columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,30 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true, 
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // References the id field in the Product model
+      references: {
+        model: 'product', 
+        key: 'id', 
+      },
+      allowNull: false, 
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // References the Tag model
+      references: {
+        model: 'tag', 
+        key: 'id',
+      },
+      allowNull: false, 
+    },
   },
   {
     sequelize,
