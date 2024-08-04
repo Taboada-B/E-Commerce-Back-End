fcd8408f6165a8d655ccdf6ad9f08edc0817997d
Bryan Taboada <b.taboada123@gmail.com>
2024-08-04 13:53:29 -0600

    got .env connected

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
-require('dotenv').config();
-
+require('dotenv').config(); 
 const Sequelize = require('sequelize');
+
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+
 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
@@ -9,7 +13,7 @@ const sequelize = process.env.DB_URL
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
-      },
-    });
+    },
+  });
 
 module.exports = sequelize;
---
+++ config/test-connection.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+  }
+);
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
